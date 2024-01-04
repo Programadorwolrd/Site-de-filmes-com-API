53b585cfdcefc63008b18a04597eea650b5a0f97
philype <lellis.sena@aluno.ifsp.edu.br>
2024-01-04 19:44:25 -0300

    melhorias no projeto

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 
 import {Outlet} from "react-router-dom";
 import Navbar from "./components/navbar";
+
 import "./App.css";
 
 function App() {
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,9 +1,24 @@
+import { useState, useEfefect, useEffect } from "react";
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    //aqui criei uma variavel chamada  getTopRatedMovies que pega o json
+    const data = await res.json();
+    setTopMovies(data.results); //mostra o data com o json
+  };
 
 
-const Home =()=>{
-    return(
-        <div>HOME</div>
-    )
-}
 
-export default Home
+  useEffect(() => {
+    const topRatedUrl =`${moviesURL}top_rated?${apiKey}`;
+  }, []);//aqui uso uma função do react chamda useEffect com outras coisas pra fazer a libveração da api com o link e a senha
+
+
+  return <div>HOME</div>;
+};
+
+export default Home;
