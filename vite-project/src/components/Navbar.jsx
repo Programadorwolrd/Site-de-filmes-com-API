06b2a8e853f901d75ba6bc002613aa6b1dce4ec0
philype <lellis.sena@aluno.ifsp.edu.br>
2024-01-09 11:25:57 -0300

    alterações e procura de um bug

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,21 +1,38 @@
-import {Link} from "react-router-dom";
-import{BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
-import './NavBar.css';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import "./NavBar.css";
 
 const Navbar = () => {
-    return (
-        <nav id="navbar">
-        <h2>
-            <Link to="/"><BiCameraMovie/>Filize</Link>
-        </h2>
-        <form >
-            <input type="text"placeholder="Busque o filme" />
-            <button type="subimit"> <BiSearchAlt2/></button>
-        </form>
-       </nav>
-    
-    
-    );
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">MoviesLib</Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+      </form>
+    </nav>
+  );
 };
 
 export default Navbar;
